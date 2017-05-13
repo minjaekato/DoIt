e5671467451b87ec9b257d10e46ddcc7a8ba1cc8
Minjae Kato <minjaekato@gmail.com>
2017-05-13 22:22:31 +0900

    Do It List

---
+++ DoIt/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by 河東ミンジェ on 2017/05/13.
+//  Copyright © 2017 Edutainers. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name = ""
+    var important = false
+}
